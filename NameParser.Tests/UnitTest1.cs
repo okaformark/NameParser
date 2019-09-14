4796ef5d273fa343e9c527b1af773f781e91e569
Mark Okafor <okaformark@gmail.com>
2019-09-14 11:06:06 -0500

    unit testing

--- NameParser.Tests/UnitTest1.cs
+++ NameParser.Tests/UnitTest1.cs
@@ -1,14 +1,33 @@
-using System;
 using Xunit;
-
 namespace NameParser.Tests
 {
-    public class UnitTest1
+    public class NameParsingTests
     {
         [Fact]
-        public void Test1()
+        public void FirstAndLastNameShouldBeParsedAccurately()
         {
+            //Arrange
+            var name = "Martin Cross";
+            var parser = new Parser();
 
+            //Act
+            var result = parser.ParseName(name);
+
+            //Assert
+            Assert.Equal("Martin", result.FirstName);
+            Assert.Equal("Cross", result.LastName);
+        }
+
+        [Fact]
+        public void ThreePartNamesShouldBeParsedAccurately()
+        {
+            var name = "Martin zachariah Cross";
+            var parser = new Parser();
+
+            var result = parser.ParseName(name);
+
+            Assert.Equal("Martin", result.FirstName);
+            Assert.Equal("Cross", result.LastName);
         }
     }
 }
---
+++ NameParser/Parser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NameParser
+{
+    public class Parser
+    {
+        public Name ParseName(string nameToParse)
+        {
+            var splitNames = nameToParse.Split(" ");
+            var name = new Name();
+            name.FirstName = splitNames.First();
+            name.LastName = splitNames.Last();
+            return name;
+        }
+    }
+
+    public class Name
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
